82437b4825830eddc2fa0533ed9032ef21f83258
wendel.silva <wendel.silva@tradein.com.br>
2024-07-02 10:26:51 -0300

    imc calculator

---
+++ calculate_imc.py
@@ -0,0 +1,6 @@
+name = "Wendel Silva"
+height = 1.65
+weight = 66
+imc = weight / (height ** 2)
+
+print(name + ' tem ' + str(height) + ' de altura,\n' + 'pesa ' + str(weight) + ' quilos e seu IMC é \n' + str(imc))
